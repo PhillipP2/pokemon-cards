7bbd9c6dbfc1b49aa7f0ca0fed6c33fc87fb792f
Phillip P <madmelonxtra@hotmail.com>
2021-12-31 20:52:30 -0700

    Updates

---
+++ precache-manifest.bd5cb4db3ca45366a5ea3cbdc811df8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "180a4b25c09bb0315ec9",
+    "url": "/pokemon-cards/static/css/main.77886f51.chunk.css"
+  },
+  {
+    "revision": "180a4b25c09bb0315ec9",
+    "url": "/pokemon-cards/static/js/main.180a4b25.chunk.js"
+  },
+  {
+    "revision": "8ee15efa1a2054ecf258",
+    "url": "/pokemon-cards/static/js/1.8ee15efa.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/pokemon-cards/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "c38ce6c2b71d8fadf23c",
+    "url": "/pokemon-cards/static/js/runtime~main.c38ce6c2.js"
+  },
+  {
+    "revision": "77035b9dc539242eb4b47b4051c90207",
+    "url": "/pokemon-cards/index.html"
+  }
+];
